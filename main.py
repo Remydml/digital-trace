6d7f23d31f0c14304f3dfb76208b40d244207882
Rémy Dumoulin <81318770+MauriceLePastis@users.noreply.github.com>
2022-12-03 16:52:58 +0100

    modif main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def hello_world():
  prefix_google = """
  <!-- Google tag (gtag.js) -->
 <script async
-src="https://www.googletagmanager.com/gtag/js?id=G-R012264CT4 "></script>
+src="https://www.googletagmanager.com/gtag/js?id=G-R012264CT4"></script>
 <script>
  window.dataLayer = window.dataLayer || [];
  function gtag(){dataLayer.push(arguments);}
