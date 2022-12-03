486c339844a3ee804d58d9d0687086b1b4e08a6c
Rémy Dumoulin <81318770+MauriceLePastis@users.noreply.github.com>
2022-12-03 16:48:03 +0100

    modif main.py

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
+
 def hello_world():
-    return "Hello World"
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=G-R012264CT4 "></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', ' YOUR_GA_CODE');
+</script>
+ """
+ return prefix_google + "Hello World"
 
 def app(event):
     return "Hello, world!"
