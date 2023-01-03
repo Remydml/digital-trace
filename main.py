b735decb2b580b934076dfee0859a3e0a44fafb6
Rémy Dumoulin <81318770+Remydml@users.noreply.github.com>
2023-01-04 00:02:57 +0100

    Add logs and cookie routes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
 
 app = Flask(__name__)
 
@@ -17,3 +19,32 @@ src="https://www.googletagmanager.com/gtag/js?id=G-R012264CT4"></script>
 </script>
  """
  return prefix_google + "Hello World"
+
+
+
+@app.route('/logger',methods=["GET"])
+def logger_page():
+    #log in the front-end and textbox
+    logs = """
+    <script>
+    console.log('This message is a log on the front end');
+    </script>
+    
+    <script>
+    alert('Text box message');
+    </script>
+    """
+    #log in the back-end
+    print('This message is a log on the back-end')
+    return logs + "Here is the logger page"
+
+
+@app.route('/cookie', methods=["GET","POST"])
+def mycookies():
+    req = request.get("https://www.google.com/")
+    return req.cookies.get_dict() 
+
+@app.route('/cookiewithga', methods=["GET","POST"])
+def mycookieganalytics():
+    req = request.get("https://analytics.google.com/analytics/web/#/p344990265/reports/intelligenthome")
+    return req.text
